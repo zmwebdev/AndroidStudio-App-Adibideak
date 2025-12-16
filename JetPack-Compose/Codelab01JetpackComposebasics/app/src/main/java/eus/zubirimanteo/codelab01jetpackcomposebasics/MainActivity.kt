3370950d34a81940cc2ff307a651cfd8a41d6b3f
zmwebdev <zmwebdev@gmail.com>
2025-12-16 10:35:56 +0100

    Lazy List

--- JetPack-Compose/Codelab01JetpackComposebasics/app/src/main/java/eus/zubirimanteo/codelab01jetpackcomposebasics/MainActivity.kt
+++ JetPack-Compose/Codelab01JetpackComposebasics/app/src/main/java/eus/zubirimanteo/codelab01jetpackcomposebasics/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -80,10 +82,10 @@ fun OnboardingScreen(
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
