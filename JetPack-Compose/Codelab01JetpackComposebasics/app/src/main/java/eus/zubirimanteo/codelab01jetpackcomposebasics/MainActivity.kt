2c889597e4d13ea76b0d1b56a1821f364b398279
zmwebdev <zmwebdev@gmail.com>
2025-12-16 09:42:37 +0100

    Jetpack Compose. Codelab 01. State in Compose

--- JetPack-Compose/Codelab01JetpackComposebasics/app/src/main/java/eus/zubirimanteo/codelab01jetpackcomposebasics/MainActivity.kt
+++ JetPack-Compose/Codelab01JetpackComposebasics/app/src/main/java/eus/zubirimanteo/codelab01jetpackcomposebasics/MainActivity.kt
@@ -15,6 +15,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,8 +29,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Codelab01JetpackComposeBasicsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android", modifier = Modifier.padding(innerPadding)
+                    MyApp(
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -51,19 +53,27 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = modifier.padding(24.dp)) {
-            Column(modifier = modifier.weight(1f)) {
+            Column(
+                modifier = modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
             ElevatedButton(
-                onClick = { /* TODO */ }
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
