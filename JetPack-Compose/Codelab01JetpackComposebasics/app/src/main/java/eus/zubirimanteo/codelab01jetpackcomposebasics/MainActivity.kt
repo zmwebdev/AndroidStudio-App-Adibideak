bc0a2caa5067a541cf69c8ae77e1036f5a34ef68
zmwebdev <zmwebdev@gmail.com>
2026-01-13 10:11:02 +0100

    Codelab 01 Jetpack Compose basics. 13. Finishing touches!

--- JetPack-Compose/Codelab01JetpackComposebasics/app/src/main/java/eus/zubirimanteo/codelab01jetpackcomposebasics/MainActivity.kt
+++ JetPack-Compose/Codelab01JetpackComposebasics/app/src/main/java/eus/zubirimanteo/codelab01jetpackcomposebasics/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -12,12 +11,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -25,11 +27,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import eus.zubirimanteo.codelab01jetpackcomposebasics.ui.theme.Codelab01JetpackComposeBasicsTheme
@@ -37,7 +39,6 @@ import eus.zubirimanteo.codelab01jetpackcomposebasics.ui.theme.Codelab01JetpackC
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             Codelab01JetpackComposeBasicsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -67,7 +68,7 @@ fun MyApp(modifier: Modifier = Modifier) {
 @Composable
 fun OnboardingScreen(
     onContinueClicked: () -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Column(
         modifier = modifier.fillMaxSize(),
@@ -87,7 +88,7 @@ fun OnboardingScreen(
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
+    names: List<String> = List(1000) { "$it" },
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
@@ -122,15 +123,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            ElevatedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+//            ElevatedButton(
+//                onClick = { expanded.value = !expanded.value }
+//            ) {
+//                Text(if (expanded.value) "Show less" else "Show more")
+//            }
+            IconButton(onClick = { expanded.value = !expanded.value }) {
+                Icon(
+                    imageVector = if (expanded.value) Filled.ExpandLess else Filled.ExpandMore,
+                    contentDescription = if (expanded.value) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
         }
     }
 }
-
 
 
 /************* Previews ******************/
