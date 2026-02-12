ee3649f212c9d333725ffe2cf7420d324d714dbd
zmwebdev <zmwebdev@gmail.com>
2026-02-12 12:56:18 +0100

    8. Favorite collections grid - Lazy grids

--- JetPack-Compose/Codelab02BasicLayoutsInCompose/app/src/main/java/eus/zubirimanteo/codelab02basiclayoutsincompose/MainActivity.kt
+++ JetPack-Compose/Codelab02BasicLayoutsInCompose/app/src/main/java/eus/zubirimanteo/codelab02basiclayoutsincompose/MainActivity.kt
@@ -11,12 +11,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -103,7 +107,7 @@ fun AlignYourBodyElement(
 fun FavoriteCollectionCard(
     @DrawableRes drawable: Int,
     @StringRes text: Int,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Surface(
         shape = MaterialTheme.shapes.medium,
@@ -131,15 +135,36 @@ fun FavoriteCollectionCard(
 
 @Composable
 fun AlignYourBodyRow(
-    modifier: Modifier = Modifier
-){
+    modifier: Modifier = Modifier,
+) {
     LazyRow(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         contentPadding = PaddingValues(horizontal = 16.dp),
         modifier = modifier
-    ){
+    ) {
         items(alignYourBodyData) { item ->
             AlignYourBodyElement(drawable = item.drawable, text = item.text)
+        }
+    }
+}
+
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier = Modifier,
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.height(168.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(
+                drawable = item.drawable,
+                text = item.text,
+                modifier = Modifier.height(80.dp)
+            )
         }
     }
 }
@@ -148,6 +173,7 @@ fun AlignYourBodyRow(
 fun MySootheApp() {
     TODO("Not yet implemented")
 }
+
 
 /****************/
 
@@ -160,9 +186,18 @@ private val alignYourBodyData = listOf(
     R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
 ).map { DrawableStringPair(it.first, it.second) }
 
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
 private data class DrawableStringPair(
     @DrawableRes val drawable: Int,
-    @StringRes val text: Int
+    @StringRes val text: Int,
 )
 
 /****************/
@@ -203,6 +238,14 @@ fun FavoriteCollectionCardPreview() {
 fun AlignYourBodyRowPreview() {
     Codelab02BasicLayoutsInComposeTheme {
         AlignYourBodyRow()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF5F0EE)
+@Composable
+fun FavoriteCollectionsGridPreview() {
+    Codelab02BasicLayoutsInComposeTheme {
+        FavoriteCollectionsGrid()
     }
 }
 
