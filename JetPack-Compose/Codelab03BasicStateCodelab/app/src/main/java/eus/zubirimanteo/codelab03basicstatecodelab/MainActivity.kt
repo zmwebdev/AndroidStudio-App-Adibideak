fe067eaaf29eacd72c88fc5159cc5d12cdda7b01
zmwebdev <zmwebdev@gmail.com>
2026-04-27 20:45:39 +0200

    7. Remember in Composition

--- JetPack-Compose/Codelab03BasicStateCodelab/app/src/main/java/eus/zubirimanteo/codelab03basicstatecodelab/MainActivity.kt
+++ JetPack-Compose/Codelab03BasicStateCodelab/app/src/main/java/eus/zubirimanteo/codelab03basicstatecodelab/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -16,8 +21,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,6 +51,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+/*
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp))
@@ -51,16 +59,71 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         //var count : MutableState<Int> = mutableStateOf(0)
         var count by rememberSaveable { mutableStateOf(0) }
         // Gemini("by" azalpena): by gako-hitzak Kotlin-i esaten dio: "Ados, count izeneko aldagai bat definituko dut. Baina bere balioa lortu (get) edo ezarri (set) nahi dudan bakoitzean, ez dut zuzenean kudeatuko. Horren ordez, lana rememberSaveable { mutableStateOf(0) }-k sortutako objektuari pasako diot".
-        Text(text = "Zuk $count ur baso dituzu")
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text(text = "Bat gehitu")
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Gaur zure 15 minutuko ibilaldia egin duzu?"
+                )
+            }
+            Text(text = "Zuk $count ur baso dituzu")
+        }
+
+        Row {
+            Button(
+                onClick = { count++ },
+                Modifier.padding(top = 8.dp),
+                enabled = count < 10
+            ) {
+                Text(text = "Bat gehitu")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp, top = 8.dp)
+            ) {
+                Text("Garbitu ur kontagailua")
+            }
+        }
+    }
+}
+*/
+
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
 }
 
+
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
+}
+
+@Composable
+fun WellnessTaskItem(
+    taskName: String, onClose: () -> Unit, modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier, verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp), text = taskName
+        )
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
+    }
 }
 
 /*********************/
@@ -69,4 +132,11 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounterPreview() {
     Codelab03BasicStateCodelabTheme() { WaterCounter() }
+}
+
+// create the Preview of the WellnessTaskItem
+@Preview(showBackground = true)
+@Composable
+fun WellnessTaskItemPreview() {
+    WellnessTaskItem(taskName = "Water me", onClose = { })
 }
